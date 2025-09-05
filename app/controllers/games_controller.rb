e713be0434db3e4bcd9d2b88914e02e875e1f532
Alistair Norman <alistair@super.gd>
2025-09-05 11:17:05 -0700

    More game crud

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   def index
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    Game.create!(game_params)
+    redirect_to games_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:opponent, :date)
+  end
 end
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -14,4 +14,26 @@ RSpec.describe GamesController, type: :request do
       expect(response.body).to include("Narwhalers")
     end
   end
+
+  describe "GET #new" do
+    subject { get "/games/new" }
+
+    it "has a form" do
+      subject
+      expect(response.body).to include("Opponent")
+    end
+  end
+
+  describe "POST #create" do
+    subject { post "/games", params: { game: { opponent: "Narwhalers", date: Date.tomorrow } } }
+
+    it "creates a game" do
+      expect { subject }
+        .to change { Game.count }
+        .from(0)
+        .to(1)
+
+      expect(response).to redirect_to(games_path)
+    end
+  end
 end
